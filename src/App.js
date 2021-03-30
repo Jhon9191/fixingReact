2d5c73dd75c2585bafe6003e2799be9b98a31281
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-29 22:21:22 -0300

    Class with states

--- src/App.js
+++ src/App.js
@@ -3,21 +3,47 @@ import './App.css';
 import { Component } from 'react'
 
 class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //this.
+  state = {
+    name: "João",
+    counter: 0
+  };
+
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  //PRECISA DE BIND PARA FUNCIONAR
+  handleClick = () => {
+    this.setState({ name: "João 2" });
+  }
+
+  //NÃO PRECISA DE BIND BUSCA DIRETO NO COMPONENT PAI
+  handleClickLink = (e) => {
+    e.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 })
+  }
+
   render() {
+    const { name, counter } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p onClick={this.handleClick}>
+            {name} {counter}
+          </p>
           <a
+            onClick={this.handleClickLink}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Link
          </a>
         </header>
       </div>
