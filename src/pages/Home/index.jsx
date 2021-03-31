83110a6c5745ea3a9648baef18359547712a7465
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-31 11:01:51 -0300

    Method for capturing search value

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,7 +11,8 @@ export class Home extends Component {
         posts: [],
         allPosts: [],
         page: 0,
-        postPerPege: 1
+        postPerPege: 1,
+        search: ''
     };
 
     async componentDidMount() {
@@ -38,11 +39,22 @@ export class Home extends Component {
         this.setState({ posts, page: nextPage })
     }
 
+    handleChange = (e) => {
+        const {value} = e.target;
+        this.setState({ search: value })
+    }
+
     render() {
-        const { posts, page, postPerPege, allPosts } = this.state;
+        const { posts, page, postPerPege, allPosts, search } = this.state;
         const noMorePosts = page + postPerPege >= allPosts.length;
         return (
             <section className="container">
+                <h1>Search value: {search}</h1>
+                <input 
+                value={search}
+                onChange={this.handleChange}
+                type="search"/>
+                <br/><br/>
                 <Posts posts={posts} />
                 <div className="button-container">
                     <Button
