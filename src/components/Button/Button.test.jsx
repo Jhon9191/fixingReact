5d2c3449c55324bad1c0cf55a3bc0857eaba277d
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-01 12:03:28 -0300

    Test to see if you have text

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -1,7 +1,15 @@
-
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import { Button } from ".";
 
 describe('<Button/>', () => {
-    
+    it('Should render the button with text "Load more"', () => {
+        render(<Button name="Load more" />);
+
+        expect.assertions(1);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+        expect(button).toBeInTheDocument();
+    });
 
 });
